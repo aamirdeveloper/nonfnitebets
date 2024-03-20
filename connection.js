e11332dbc3ace1bebc4fc5dc42d83d1f24f56c49
aamirdeveloper <aamir.0019@gmail.com>
2024-03-20 10:47:45 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 let mysql = require('./node_modules/mysql2');
 
 let connection = mysql.createConnection({
-    host: 'srv1098.hstgr.io',
+    host: '185.232.14.52',
     user: 'u498357097_settrade',
     password: '7WDzrvxdMLPjt',
     database: 'u498357097_nonfinitebets'
