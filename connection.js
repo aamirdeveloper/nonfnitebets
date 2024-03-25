e995888b8eab3707b3c2125155db08ea0d18e599
Nodejs <aamir.0019@gmail.com>
2024-03-25 20:07:17 +0530

    db

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ let mysql = require('./node_modules/mysql2');
 
 let connection = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'bets',
+    user: 'bets@localhost',
     password: 'o$65I3tKUl',
     database: 'nonfinite'
 });
