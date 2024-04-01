16724d6ccead77dd1ff3d06046654257de08c56f
Nodejs <aamir.0019@gmail.com>
2024-04-01 11:42:42 +0530

    a

--- models/user.js
+++ models/user.js
@@ -22,6 +22,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     //freezeTableName: true,
+    freezeTableName: true,
+    tableName: 'users',
     modelName: 'User',
   });
   return User;
